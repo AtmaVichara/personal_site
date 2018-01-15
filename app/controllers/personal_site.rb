48e5c2054b4dfa966648c31aed68a72b4696e730
Joe Jobes <jmrjobes@gmail.com>
2018-01-15 10:24:15 -0700

    Implementing changes to personal_site for error page

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -2,6 +2,17 @@ require 'rack'
 
 class PersonalSite
   def self.call(env)
+    case env["PATH_INFO"]
+    when '/' then index
+    else
+      error
+  end
+
+  def self.index
     ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]]
+  end
+
+  def self.error
+    ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/error.html')]]
   end
 end
